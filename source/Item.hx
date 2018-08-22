100fb51f6d43d71b163ab9450a4ad933d842b6da
Julian <julian.dinges@men.de>
2018-08-22 07:13:15 +0200

    Added item modifiers

--- source/Item.hx
+++ source/Item.hx
@@ -9,11 +9,23 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
  */
 class Item extends FlxSprite
 {
+	// Modifiers are additive, multipliers... guess what
+
+	public var evasionModifier       : Float = 0;
+	public var walkSpeedModifier     : Float = 0;
+	public var dashDistanceModifier  : Float = 0;
+	public var dashCooldownModifier  : Float = 0;
+	
+	public var damageMultiplier      : Float = 0;
+	public var attackSpeedMultiplier : Float = 0;
+	public var critChanceModifier    : Float = 0;
+	public var critDamageModifier    : Float = 0;
+	public var lifeLeechModifier     : Float = 0;
 
 	public function new() 
 	{
 		super();
-		
+
 	}
 	
 }
