26087ac269b686e098f670689b97d28217b45c49
Thunraz <thunraz@thunraz.com>
2018-08-25 19:06:04 +0200

    Added var for item tier

--- source/Item.hx
+++ source/Item.hx
@@ -11,6 +11,7 @@ class Item extends FlxSprite
 {
 	public var name : String;
 	public var type : ItemType;
+	public var tier : Int;
 
 	public var evasionMultiplier      : Float = 1.0;
 	public var walkSpeedMultiplier    : Float = 1.0;
@@ -35,6 +36,7 @@ class Item extends FlxSprite
 		var item  = new Item();
 		item.name = 'Short Sword';
 		item.type = ItemType.SWORD;
+		item.tier = 1;
 		item.loadGraphic(AssetPaths.shortsword__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.1;
@@ -55,6 +57,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Dagger';
 		item.type = ItemType.SWORD;
+		item.tier = 2;
 		item.loadGraphic(AssetPaths.dagger__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.5;
@@ -75,6 +78,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Claymore';
 		item.type = ItemType.SWORD;
+		item.tier = 2;
 		item.loadGraphic(AssetPaths.claymore__png, true, 16, 16);
 
 		item.evasionMultiplier      = 0.9;
@@ -95,6 +99,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Katana';
 		item.type = ItemType.SWORD;
+		item.tier = 3;
 		item.loadGraphic(AssetPaths.katana__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -118,6 +123,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Robe';
 		item.type = ItemType.ARMOR;
+		item.tier = 1;
 		item.loadGraphic(AssetPaths.robe__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -138,6 +144,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Leather Armor';
 		item.type = ItemType.ARMOR;
+		item.tier = 2;
 		item.loadGraphic(AssetPaths.leatherarmor__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -158,6 +165,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Chain Mail';
 		item.type = ItemType.ARMOR;
+		item.tier = 2;
 		item.loadGraphic(AssetPaths.chainmail__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -178,6 +186,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Plate Mail';
 		item.type = ItemType.ARMOR;
+		item.tier = 3;
 		item.loadGraphic(AssetPaths.platemail__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -201,6 +210,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Selfbow';
 		item.type = ItemType.BOW;
+		item.tier = 1;
 		item.loadGraphic(AssetPaths.selfbow__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -221,6 +231,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Longbow';
 		item.type = ItemType.BOW;
+		item.tier = 2;
 		item.loadGraphic(AssetPaths.longbow__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -241,6 +252,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Recurve bow';
 		item.type = ItemType.BOW;
+		item.tier = 2;
 		item.loadGraphic(AssetPaths.recurvebow__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
@@ -261,6 +273,7 @@ class Item extends FlxSprite
 		var item = new Item();
 		item.name = 'Crossbow';
 		item.type = ItemType.BOW;
+		item.tier = 3;
 		item.loadGraphic(AssetPaths.crossbow__png, true, 16, 16);
 
 		item.evasionMultiplier      = 1.0;
