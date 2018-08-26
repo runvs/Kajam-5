e7a15708cb918f29ac85ab776f165a93585ca985
Thunraz <thunraz@thunraz.com>
2018-08-26 15:24:41 +0200

    Commenting out background for now

--- source/PlayerInfo.hx
+++ source/PlayerInfo.hx
@@ -6,32 +6,38 @@ import flixel.FlxSprite;
 class PlayerInfo extends FlxSprite
 {
 	var _player     : Player;
-	var _background : FlxSprite;
+	// var _background : FlxSprite;
 	
 	public function new(player : Player)
 	{
 		super();
+
+		x = 100;
+		y = 100;
+
+		makeGraphic(100, 100, FlxColor.MAGENTA);
 		
-		origin.set(0, 0);
+		//origin.set(0, 0);
 		scrollFactor.set();
 
-		_background = new FlxSprite(10, 10);
-		_background.makeGraphic(100, 100, FlxColor.fromRGB(64, 64, 64, 100));
-		_background.scrollFactor.set();
+		// _background = new FlxSprite(10, 10);
+		// _background.makeGraphic(100, 100, FlxColor.fromRGB(64, 64, 64, 100));
+		// _background.scrollFactor.set();
 
 		_player = player;
 
 		visible = true;
 	}
 	
-	override public function update(elapsed:Float):Void
+	public override function update(elapsed : Float) : Void
 	{
 		super.update(elapsed);
+		// _background.update(elapsed);
 	}
 
 	public override function draw() : Void 
 	{
-		_background.draw();
+		// _background.draw();
 		super.draw();
 	}
 }
