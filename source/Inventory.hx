fd1aae07a1c41cf71f1b7eeb77bd1fe1f79b6bc6
Simon Weis <laguna.ws@gmail.com>
2018-09-01 11:42:59 +0200

    checking if items are available

---
+++ source/Inventory.hx
@@ -0,0 +1,30 @@
+package;
+
+/**
+ * ...
+ * @author 
+ */
+class Inventory
+{
+
+	public var hasBoughtShortSword : Bool = true;
+	public var hasBoughtDagger : Bool = false;
+	public var hasBoughtKatana : Bool = false;
+	public var hasBoughtClaymore : Bool = false;
+	
+	public var hasBoughtRobe : Bool = true;
+	public var hasBoughtLeatherArmor : Bool = false;
+	public var hasBoughtChainMail : Bool = false;
+	public var hasBoughtPlateMail : Bool = false;
+	
+	public var hasBoughtSelfBow : Bool = true;
+	public var hasBoughtLongBow : Bool = false;
+	public var hasBoughtCrossbow : Bool = false;
+	public var hasBoughtRecurveBow : Bool = false;
+	
+	public function new() 
+	{
+		
+	}
+	
+}
